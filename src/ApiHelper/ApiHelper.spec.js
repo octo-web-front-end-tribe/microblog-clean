a2073893572679ef3b7d16439c61cde4585559b2
Roman Quelen <rquelen@octo.com>
2017-01-28 17:04:13 +0100

    fetch messages from API and display message list

--- src/ApiHelper/ApiHelper.spec.js
+++ src/ApiHelper/ApiHelper.spec.js
@@ -1,32 +1,32 @@
 import chai from 'chai';
 import fetchMock from 'fetch-mock';
 import chaiFetchMock from 'chai-fetch-mock';
-import { fetchMessages } from './ApiHelper';
+import {fetchMessages} from './ApiHelper';
 
 const { expect } = chai;
 chai.use(chaiFetchMock);
 
 const messages = [
-  { id: 'abcd', content: 'fakeContent', author: 'fakeAuthor' },
-];
+  { id: 'abcd', content: 'fakeContent', author: 'fakeAuthor' }
+]
 
 describe('ApiHelper component', () => {
-  const getMessageRoute = 'https://skool-microblog.herokuapp.com/messages';
+  const getMessageRoute = 'https://skool-microblog.herokuapp.com/messages'
 
   describe('when the query is successful', () => {
     beforeEach(() => (
       fetchMock.get(getMessageRoute, messages)
-    ));
+    ))
 
-    afterEach(fetchMock.restore);
+    afterEach(fetchMock.restore)
 
     it('should return a response with the expected messages', () => {
       return fetchMessages()
         .then((response) => {
           /* eslint no-unused-expressions:0 */
-          expect(fetchMock).route(getMessageRoute).to.have.been.calledOnce;
+          expect(fetchMock).route(getMessageRoute).to.have.been.calledOnce
           expect(response).to.deep.equal(messages);
-        });
-    });
-  });
-});
+        })
+    })
+  })
+})
