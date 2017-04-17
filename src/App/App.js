7dabe9ce1f7ef26079818146e4f8571f492dec61
Pierrette Bertrand <pbertrand@octo.com>
2017-04-17 17:11:11 +0200

    clean

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import MessageList from '../MessageList/MessageList';
 import { container, messageBox } from './App.css';
 
-const App = ({ name }) => (
+const App = () => (
   <div className={container}>
-    <div className={[messageBox, 'greeting'].join(' ')}>{`Hello ${name} !`}</div>
-    <div className={messageBox}>
-      <MessageList
-        messages={[
-        { id: '1', author: 'anass', content: 'JS is amazing' },
-        { id: '2', author: 'pierrette', content: 'JS is impressive' },
-        ]}
-      />
-    </div>
+    <div className={[messageBox, 'greeting'].join(' ')}>Hello</div>
   </div>
 );
-
-App.propTypes = {
-  name: PropTypes.string,
-};
-
-App.defaultProps = {
-  name: 'OCTO',
-};
 
 export default App;
--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -2,24 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import App from './App';
-import MessageList from '../MessageList/MessageList';
 
 describe('App component', () => {
-  it('should render hello message', () => {
+  it('should render a div with right className', () => {
     const wrapper = shallow(<App />);
 
-    expect(wrapper.find('.greeting').text()).to.equal('Hello OCTO !');
-  });
-
-  it('Should render hello message with the right props', () => {
-    const wrapper = shallow(<App name={'myName'} />);
-
-    expect(wrapper.find('.greeting').text()).to.equal('Hello myName !');
-  });
-
-  it('Should render a MessageList component', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper.find(MessageList)).to.have.length(1);
+    expect(wrapper.find('.greeting').text()).to.equal('Hello');
   });
 });
