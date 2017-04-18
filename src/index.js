5d60fdae7dd4bef8e947b815b1d3aed956aac7e9
Anass Oumiri <aoumiri@octo.com>
2017-04-18 23:07:04 +0200

    Wrap application into redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import Routes from './Routes/Routes';
+import LoginReducer from './Store/Login/LoginReducer';
+
+const store = createStore(LoginReducer);
 
 ReactDOM.render(
-  <Routes />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root'),
 );
