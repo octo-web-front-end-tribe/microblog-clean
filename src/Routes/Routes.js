ab933ca67504f3bdc906245b62fead78e0192cc4
Anass Oumiri <aoumiri@octo.com>
2017-04-19 10:29:22 +0200

    User LoginContainer instead of Login in Routes.js

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,14 +3,14 @@ import { HashRouter, Route } from 'react-router-dom';
 import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute';
 import App from '../App/App';
 import About from '../About/About';
-import Login from '../Login/Login';
+import LoginContainer from '../Store/Login/LoginContainer';
 
 export default () => (
   <HashRouter hashType="slash">
     <div>
       <Route exact path="/" render={() => <AuthenticatedRoute><App /></AuthenticatedRoute>} />
       <Route path="/about" render={() => <AuthenticatedRoute><About /></AuthenticatedRoute>} />
-      <Route path="/login" component={Login} />
+      <Route path="/login" component={LoginContainer} />
     </div>
   </HashRouter>
 );
